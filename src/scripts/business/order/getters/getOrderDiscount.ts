f57199c5dd62fab07b77f65291a3c5aa70ffba16
kenkz447 <kenkz447@gmail.com>
2019-04-02 10:38:33 +0700

    update

--- src/scripts/business/order/getters/getOrderDiscount.ts
+++ src/scripts/business/order/getters/getOrderDiscount.ts
@@ -10,16 +10,15 @@ export const getOrderDiscount = (order: Partial<Order>, subTotal?: number) => {
         agencyOrderer,
         promotion
     } = order;
-    
+
     if (!subTotal) {
         subTotal = getOrderDetailsTotal(orderDetails);
     }
-    
+
     const promotionDiscount = promotion ? (promotion.discountPrice || 0) : 0;
     const productDiscount = getOrderDetailsDiscount(orderDetails);
 
-    const agencyDiscount = (subTotal -
-        (promotionDiscount + productDiscount)) * (agencyOrderer!.level.discountPercent * 0.01);
+    const agencyDiscount = (subTotal - productDiscount) * (agencyOrderer!.level.discountPercent * 0.01);
 
     const totalDiscount = promotionDiscount + productDiscount + agencyDiscount;
 
